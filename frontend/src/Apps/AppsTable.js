39b086745157013ad743503d8717ed24713de18d
mattbf <matthewbf8@gmail.com>
2019-04-30 16:03:06 -0600

    Formated data into a table

--- frontend/src/Apps/AppsTable.js
+++ frontend/src/Apps/AppsTable.js
@@ -13,12 +13,46 @@ function AppsTable() {
 
   return (
     <div>
-      Table
-      {AppsData.map( app =>
-        <div key={app.id}> Name: {app.name} </div>
-      )}
+    <table  className="table">
+         <thead  key="thead">
+         <tr>
+             <th>#</th>
+             <th>App</th>
+             <th>Website</th>
+             <th>launchYear</th>
+             <th>Service Type</th>
+             <th>Business Model</th>
+             <th>Description</th>
+             <th>Monitization</th>
+             <th>Tags</th>
+             <th>createdOn</th>
+         </tr>
+         </thead>
+         <tbody>
+         {AppsData.map( app  =>
+             <tr  key={app.id}>
+             <td>{app.id}  </td>
+             <td>{app.name}</td>
+             <td><a href={app.website}> {app.website} </a></td>
+             <td>{app.launchYear}</td>
+             <td>{app.serviceType}</td>
+             <td>{app.businessModel}</td>
+             <td>{app.description}</td>
+             <td>{app.monitization}</td>
+             <td>{app.tags}</td>
+             <td>{app.createdOn}</td>
+             <td>
+             <button > Delete</button>
+             <a  href={"/app/" + app.id}> Update</a>
+             </td>
+         </tr>)}
+         </tbody>
+         </table>
+         <button  className="btn btn-primary">Next</button>
     </div>
   )
 }
 
 export default AppsTable
+
+ //onClick={(e)=>  this.handleDelete(e,app.id) }
